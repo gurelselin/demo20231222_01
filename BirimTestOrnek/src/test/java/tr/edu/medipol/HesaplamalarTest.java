e8ec7ee3eb6cd3ded5d8337dae40639ad1c32164
gurelselin <selin.gurel@std.medipol.edu.tr>
2023-12-23 14:04:22 +0300

    Birim Test Ornek paylasiliyor

---
+++ BirimTestOrnek/src/test/java/tr/edu/medipol/HesaplamalarTest.java
@@ -0,0 +1,51 @@
+package tr.edu.medipol;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HesaplamalarTest {
+
+	@Test
+	public void testFaktoriyelRec() {
+		int sonuc = Hesaplamalar.faktoriyelRec(5);
+		assertEquals(120, sonuc);
+	}
+	
+	@Test
+	public void testFaktoriyelLoop() {
+		int sonuc = Hesaplamalar.faktoriyelLoop(5);
+		assertEquals(120, sonuc);
+	}
+
+	@Test
+	 public void testFaktoriyel() {
+		for(int i=1; i<1000; i++) {
+		int sonuc1= Hesaplamalar.faktoriyelLoop(i);
+		int sonuc2= Hesaplamalar.faktoriyelRec(i);
+		assertEquals(sonuc1,sonuc2);
+				
+		}
+	
+	}
+	
+	@Test
+	public void testTopla() {
+		int sonuc = Hesaplamalar.topla(17, 5);
+		assertEquals(22, sonuc);
+	}
+	
+	@Test
+	public void testCikart() {
+		int sonuc = Hesaplamalar.cikart(17, 5);
+		assertEquals(12, sonuc);
+	} 
+	
+	@Test
+	public void testCarp() {
+		int sonuc = Hesaplamalar.carp(17, 5);
+		assertEquals(85, sonuc);
+	}
+	
+	
+}
